a41e63862d27adce7f6a131d4fad5be15e50840b
J. Allen Baron <jabaron.phd@gmail.com>
2024-09-12 15:17:53 -0400

    Finalize word_similar()
    1. Drop output names to avoid issues they caused in dplyr::mutate.
    2. Add option to stem words before comparison.
    3. Make calculation of word similarity more clear with use of
    intersect()/union().

--- scripts/function-word_similar.R
+++ scripts/function-word_similar.R
@@ -3,23 +3,32 @@
 #' @param ignore A logical vector indicating which values of x & y to ignore;
 #' `FALSE` (default) means ignore nothing.
 #' @param stopwords A character vector of stop words (words to be ignored).
+#' @param stem_word Whether to perform word stemming or not, as a boolean
+#' (default: `FALSE`)
 #' @inheritDotParams tokenizers::tokenize_words
-word_similar <- function(x, y, ignore = FALSE, stopwords = NULL, ...) {
+word_similar <- function(x, y, ignore = FALSE, stopwords = NULL,
+                         stem_words = FALSE, ...) {
     stopifnot("length of `y` must be the same as `x`" = length(y) == length(x))
 
     xtest <- x[!ignore]
     ytest <- y[!ignore]
 
-    # retaining stop words
-    xw <- tokenizers::tokenize_words(xtest, stopwords = stopwords, ...)
-    yw <- tokenizers::tokenize_words(ytest, stopwords = stopwords, ...)
-    all <- purrr::map2(xw, yw, union)
+    if (stem_words) {
+        xw <- tokenizers::tokenize_word_stems(xtest, stopwords = stopwords, ...)
+        yw <- tokenizers::tokenize_word_stems(ytest, stopwords = stopwords, ...)
+    } else {
+        xw <- tokenizers::tokenize_words(xtest, stopwords = stopwords, ...)
+        yw <- tokenizers::tokenize_words(ytest, stopwords = stopwords, ...)
+    }
 
-    pct_sim <- purrr::pmap_dbl(
-        list(xw, yw, all),
-        function(.x, .y, .a) round(sum(.x %in% .y) / length(.a) * 100, 2)
+    pct_sim <- purrr::map2_dbl(
+        xw,
+        yw,
+        function(.x, .y, .a) {
+            round(length(intersect(.x, .y)) / length(union(.x, .y)) * 100, 2)
+        }
     ) |>
         purrr::set_names(xtest)
 
-    pct_sim[x]
+    unname(pct_sim[x])
 }
